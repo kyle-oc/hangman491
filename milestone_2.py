e15dd972632ffd68d5fdfa29234538a617b3f15d
Kyle O'Callaghan <kc_ocallaghan@hotmail.co.uk>
2024-08-11 21:27:54 +0100

    Updates and refactors some code in milestone_2.py

--- milestone_2.py
+++ milestone_2.py
@@ -3,22 +3,20 @@ import random
 word_list = ["bananas", "blueberries", "oranges", "mangos", "watermelon"]
 print(word_list)
 
-word = random.choice(word_list)
-print(word)
+random_word = random.choice(word_list)
+print(random_word)
 
 
-# guess = "anus"
+# guess = "guess"
 # while len(guess) != 1 and guess.isalnum:
 #     print("That's not a valid entry")
 #     guess = str(input("Please enter your guess: "))
 # else:
 #     print(guess)
 
-guess = input("Please enter your guess: ")
-if len(guess) == 1 and guess.isalnum:
-    print("Good guess, dingus!")
+user_guess = input("Please enter your guess: ")
+if len(user_guess) == 1 and user_guess.isalnum:
+    print("Good guess!")
 else:
     print("Oops! That is not a valid input")
-
-
 
